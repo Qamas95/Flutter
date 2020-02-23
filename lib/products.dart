e0aa0dd6bbece6e4976b3bcc1803de6be7823279
Qamas95 <kamil.p.kubiak@gmail.com>
2020-02-23 16:45:57 +0100

    Update

--- lib/products.dart
+++ lib/products.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatelessWidget {
- final List<String> products;
-  Products(this.products);
+  final List<String> products;
+  Products(this.products) {
+    print('[Products Widget] Constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map(
