0a9b0b894b5c0c45f1000fa94793b3c3ebcfc61b
Qamas95 <kamil.p.kubiak@gmail.com>
2020-02-29 12:59:56 +0100

    Doin stuff on buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 
-import './text_control.dart';
+// void main() {
+//   runApp(MyApp());
+// }
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Assignment 1'),
+          title: Text('My first App'),
         ),
-        body: TextControl(),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Ansewr 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Ansewr 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Ansewr 3'),
+              onPressed: () {
+                // ... code 
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
