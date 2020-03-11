7eb66066ff0fa20e9769162544e4aa2d28cb7d11
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-03-11 22:08:53 +0100

    Create new app to control money flow.

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,24 @@
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favourite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'What\'s your favourite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 10},
-        {'text': 'Snake', 'score': 5},
-        {'text': 'Elephant', 'score': 3},
-        {'text': 'Lion', 'score': 1},
-      ],
-    },
-    {
-      'questionText': 'Who is your favourite raper',
-      'answers': [
-        {'text': '50 Cent', 'score': 10},
-        {'text': 'JayZ', 'score': 5},
-        {'text': 'SnoopDog', 'score': 3},
-        {'text': 'Eminem', 'score': 1},
-      ],
-    }
-  ];
-
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions!');
-    } else {
-      print('No more questions!');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
-              )
-            : Result(_totalScore, _resetQuiz),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(
+        child: Text('Widget Playground!'),
       ),
     );
   }
