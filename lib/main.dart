37da539d49859d5d3919219a1bbd8896c9134724
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-03-16 22:15:09 +0100

    Add TextFields and FlatButton to enter entered data.

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -46,6 +46,30 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('CHART!'),
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+
+                    },
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                  )
+                ],
+              ),
             ),
           ),
           Column(
@@ -86,7 +110,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          DateFormat('yyyy/MM/dd').format(tx.date),
+                          DateFormat.jms().format(tx.date),
                           style: TextStyle(
                             color: Colors.grey,
                             fontSize: 15,
--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 
 class Transaction {
@@ -13,3 +15,4 @@ class Transaction {
     @required this.date,
   });
 }
+
