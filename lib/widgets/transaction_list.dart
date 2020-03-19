f31ac40e53e0ce6b765a826f35cd69092cb77ae3
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-03-19 23:44:17 +0100

    Add some logic

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -28,7 +28,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '\$${transactions[index].amount}', //+ tx.amount.toString(),
+                    '\$${transactions[index].amount.toStringAsFixed(2)}', //+ tx.amount.toString(),
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
