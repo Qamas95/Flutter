9cb3fa6ccafe164801fe56ae33413f0d0a6b63d3
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-04-15 18:01:04 +0200

    Add way to choose date in transactions.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,7 @@
+//import 'dart:js';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -10,27 +13,46 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-
-  final amountController = TextEditingController();
-
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
-
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
-      return;
-    }
-
-    widget.addTx(
-      enteredTitle,
-      enteredAmount,
-    );
-    // Navigator.of(context).pop();  // Not working ;/ should close window of enter values after click ok, but needs to click add transactions
-  }
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
   @override
   Widget build(BuildContext context) {
+    void _submitData() {
+      final enteredTitle = _titleController.text;
+      final enteredAmount = double.parse(_amountController.text);
+
+      if (enteredTitle.isEmpty || enteredAmount <= 0) {
+        return;
+      }
+
+      widget.addTx(
+        enteredTitle,
+        enteredAmount,
+      );
+      Navigator.of(context)
+          .pop(); // Not working ;/ should close window of enter values after click ok, but needs to click add transactions
+    }
+
+    void _presentDatePicker() {
+      showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2020),
+        lastDate: DateTime.now(),
+      ).then((pickedDay) {
+        if (pickedDay == null) {
+          return;
+        }
+        setState(() {
+             _selectedDate = pickedDay;
+        });
+     
+      });
+      print('.....');
+    }
+
     return Card(
       elevation: 5,
       child: Container(
@@ -40,17 +62,17 @@ class _NewTransactionState extends State<NewTransaction> {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData,
+              controller: _titleController,
+              onSubmitted: (_) => _submitData,
               // onChanged: (val) {
               //   titleInput = val;
               // },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
+              controller: _amountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData,
+              onSubmitted: (_) => _submitData,
               // onChanged: (val) {
               //   amountInput = val;
               // },
@@ -59,10 +81,16 @@ class _NewTransactionState extends State<NewTransaction> {
               height: 70,
               child: Row(
                 children: <Widget>[
-                  Text('No Date Chosen!'),
+                  Expanded(
+                                      child: Text(
+                      _selectedDate == null
+                          ? 'No Date Chosen!'
+                          : DateFormat.yMd().format(_selectedDate),
+                    ),
+                  ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    onPressed: () {},
+                    onPressed: _presentDatePicker,
                     child: Text(
                       'Choose Date',
                       style: TextStyle(
@@ -77,7 +105,7 @@ class _NewTransactionState extends State<NewTransaction> {
               child: Text('Add Transaction'),
               color: Theme.of(context).primaryColor,
               textColor: Theme.of(context).textTheme.button.color,
-              onPressed: submitData,
+              onPressed: _submitData,
             ),
           ],
         ),
